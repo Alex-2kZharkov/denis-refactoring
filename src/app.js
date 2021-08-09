406451429ace2095704146aa770f79ccd85ecdd9
Alexander Zharkov <alexander.06.zharkov@gmail.com>
2021-08-09 23:36:02 +0600

    Simplified scraping logic + delete original index.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const {scrape} = require("./scrapers");
 const { eBayScraperConfigs } = require("./configs/scrapers");
 const { amazonScraperConfigs } = require("./configs/scrapers");
 const { NO_CONCURRENT_BROWSERS } = require("./utils/constant");
@@ -5,11 +6,12 @@ const {
   capabilitiesChrome,
   capabilitiesFirefox, // moving configs to their own folders
 } = require("./configs/capabilities");
-const { scrape } = require("./scrapers/Scraper");
 
-for (let i = 0; i < NO_CONCURRENT_BROWSERS; i++) {
-  const caps =
-    i < NO_CONCURRENT_BROWSERS / 2 ? capabilitiesChrome : capabilitiesFirefox; // short version of condition ? true : false
-  await scrape({ ...amazonScraperConfigs, capabilities: caps }); // {...} - creates new object, last property "capabilities" will rewrite already existed property "capabilities"
-  await scrape({ ...eBayScraperConfigs, capabilities: caps });
-}
+
+// for (let i = 0; i < NO_CONCURRENT_BROWSERS; i++) {
+//   const caps =
+//     i < NO_CONCURRENT_BROWSERS / 2 ? capabilitiesChrome : capabilitiesFirefox; // short version of condition ? true : false
+//   await scrape({ ...amazonScraperConfigs, capabilities: caps }); // {...} - creates new object, last property "capabilities" will rewrite already existed property "capabilities"
+//   await scrape({ ...eBayScraperConfigs, capabilities: caps });
+// }
+console.log('HELLO WORLD')
